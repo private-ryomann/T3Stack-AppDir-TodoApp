294e9c3139e5e676ce52c1a50d6acbbf25902a6f
private-ryomann <ryomann46497@gmail.com>
2023-09-09 11:37:18 +0900

    T3Stack-Appdirテンプレート作成

---
+++ frontend/dev.Dockerfile
@@ -0,0 +1,14 @@
+FROM node:18-slim
+
+WORKDIR /app
+
+RUN apt-get update -y && apt-get install -y openssl
+
+COPY ./frontend/package.json ./frontend/package-lock.json ./
+
+RUN npm ci
+
+COPY ./frontend .
+
+# CMD npm run dev
+CMD tail -F /dev/null
---
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID ?? "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
